b4fa864d2160809714203fa28ec121242a4d535b
SickieGit <66867142+SickieGit@users.noreply.github.com>
2023-11-14 19:44:23 +0100

    split html, css, js

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js
+function filterGames() {
+  var input, filter, grid, gridItems, gameTitle, i;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  grid = document.getElementById("gameGrid");
+  gridItems = grid.getElementsByClassName("grid-item");
+
+  for (i = 0; i < gridItems.length; i++) {
+    gameTitle = gridItems[i].getElementsByTagName("p")[0];
+    if (gameTitle.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      gridItems[i].style.display = "";
+    } else {
+      gridItems[i].style.display = "none";
+    }
+  }
+}
